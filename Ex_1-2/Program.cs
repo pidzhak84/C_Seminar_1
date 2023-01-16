f1d98132aebf7877af102cbe43f64c825648a102
pidzhak <pidzhak84@gmail.com>
2023-01-16 16:00:36 +0300

    Решение Задачи 1.2

---
+++ Ex_1-2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.  5 -> Пятница 
+
+Console.Clear();
+
+Console.Write("Введите номер - ");
+int day = int.Parse(Console.ReadLine()!);
+
+if (day == 1)
+{
+    Console.WriteLine("Day - пн");
+}
+else if (day == 2)
+{
+    Console.WriteLine("Day - вт");
+}
+else if (day == 3)
+{
+    Console.WriteLine("Day - ср");
+}
+else if (day == 4)
+{
+    Console.WriteLine("Day - чт");
+}
+else if (day == 5)
+{
+    Console.WriteLine("Day - пт");
+}
+else if (day == 6)
+{
+    Console.WriteLine("Day - сб");
+}
+else if (day == 7)
+{
+    Console.WriteLine("Day - вс");
+}
