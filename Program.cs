993bceac5cfead34dbe84bf34206524fcd1c968c
pidzhak <pidzhak84@gmail.com>
2023-01-16 14:01:06 +0300

    Решение Задачи 0

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int sqr = number * number;
+
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
