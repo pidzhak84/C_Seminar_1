9b1679d6fd3baee71ce1bf7d9d77477ac542c0f8
pidzhak <pidzhak84@gmail.com>
2023-01-17 13:31:29 +0300

    Решение Задачи 7

---
+++ Ex_7/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру
+// этого числа. 456 -> 6, 782 -> 2, 918 -> 8
+
+Console.Clear();
+
+Console.Write("Эй, ты, число введи ");
+int number = int.Parse(Console.ReadLine()!);
+if(number<0)
+{number = number * (-1);}
+Console.WriteLine(number%10);
